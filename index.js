8d26599b0fe32a225859bd58a4586a8683dbbdf7
zhangcheng <358731817@qq.com>
2017-09-25 16:32:42 +0800

    初始化机器人

---
+++ index.js
@@ -0,0 +1,31 @@
+//387786311:AAHPSBye586ZGGjowN68n3jXK93EQR5qm-A
+
+
+const TelegramBot = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '387786311:AAHPSBye586ZGGjowN68n3jXK93EQR5qm-A';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message2');
+});
