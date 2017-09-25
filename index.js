aa0dbb65785a41c5a3ac0df42de77a8cd0310fba
zhangcheng <358731817@qq.com>
2017-09-25 16:36:06 +0800

    增加license命令

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const token = '387786311:AAHPSBye586ZGGjowN68n3jXK93EQR5qm-A';
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
+// Matches "/license [whatever]"
+bot.onText(/\/license/, (msg, match) => {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
@@ -19,7 +19,7 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
   const resp = match[1]; // the captured "whatever"
 
   // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
+  bot.sendMessage(chatId, '给你一个序列号');
 });
 
 
@@ -27,5 +27,5 @@ bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message2');
+  bot.sendMessage(chatId, msg.text);
 });
